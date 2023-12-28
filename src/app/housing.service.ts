2a4dd1d26138e8939f254f36cd4716a4b8b5064e
Hậu Trần <110884738+hautran-02@users.noreply.github.com>
2023-12-28 22:51:29 +0700

    http pratice

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -6,6 +6,8 @@ import { HousingLocation } from './housinglocation';
 })
 export class HousingService {
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+  url = 'http://localhost:3000/locations';
+
   housingLocationList: HousingLocation[] = [
     {
       id: 0,
@@ -111,13 +113,21 @@ export class HousingService {
 
   constructor() {}
 
-  getAllHousingLocations(): HousingLocation[] {
-    return this.housingLocationList;
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return (await data.json()) ?? [];
   }
 
-  getHousingLocationById(id: number): HousingLocation | undefined {
-    return this.housingLocationList.find(
-      (housingLocation) => housingLocation.id === id
+  async getHousingLocationById(
+    id: number
+  ): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return (await data.json()) ?? {};
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`
     );
   }
 }
